7bf71ade5884295244434a992e4669ebb5eebbdd
Andrii <andriypetrnko@gmail.com>
2024-01-17 16:09:17 +0100

    sadadds

--- projekt_programowanie/Form1.cs
+++ projekt_programowanie/Form1.cs
@@ -30,14 +30,21 @@ namespace projekt_programowanie
             string ConString = "Data Source = (localDB)\\MSSQLLocalDB; Initial Catalog = LocalDB; Integrated Security = true";
             SqlConnection con = new SqlConnection(ConString);
             con.Open();
-            string query = $"INSERT INTO Customers VALUES (\'{textBox1.Text.ToString()}\',\'{textBox2.Text.ToString()}\');" +
-                $"SELECT * FROM Customers WHERE Imie = \'{textBox1.Text.ToString()}\'";
+            string query = $"INSERT INTO Customers(FirstName, LastName) VALUES (\'{textBox1.Text.ToString()}\',\'{textBox2.Text.ToString()}\');" +
+                $"SELECT * FROM Customers WHERE FirstName = \'{textBox1.Text.ToString()}\'";
             SqlCommand cmd = new SqlCommand(query, con);
             SqlDataReader r = cmd.ExecuteReader();
             while (r.Read())
             {
-                MessageBox.Show($"{r.GetValue(0).ToString()} : {r.GetValue(1).ToString()}");
+                MessageBox.Show($"{r.GetValue(3).ToString()} : {r.GetValue(4).ToString()}");
             }
+            OpenSecondForm();
+            
+        }
+        void OpenSecondForm()
+        {
+            Form form2 = new Form();
+            form2.ShowDialog();
         }
     }
 }
